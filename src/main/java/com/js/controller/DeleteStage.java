8ad0eefe38a6eb73cfc7b202fdbe41814e246566
Anushreearu <anushreeshiv19@gmail.com>
2023-08-05 16:26:30 +0530

    my first commit

---
+++ src/main/java/com/js/controller/DeleteStage.java
@@ -0,0 +1,32 @@
+package com.js.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.js.dao.ProductCRUD;
+@WebServlet("/deleteBrand")
+public class DeleteStage extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	String brand=req.getParameter(" brand");
+	ProductCRUD pc = new ProductCRUD();
+	int res=pc.deleteProductByBrand(brand);
+	if(res>0) {
+		RequestDispatcher rd=req.getRequestDispatcher("view");
+        rd.forward(req,resp);
+	}else {
+		RequestDispatcher rd=req.getRequestDispatcher("result.jsp");
+		req.setAttribute("msg", "Failed to delete product");
+		rd.forward(req, resp);
+	}
+	}
+}
+	
+
+
